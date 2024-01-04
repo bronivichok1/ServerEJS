2329051c67b497880015ee2acbbcc966d052d6f5
L.A <lazarenkov01@gmail.com>
2024-01-05 01:03:06 +0300

    0.0.1 version

--- index.js
+++ index.js
@@ -3,12 +3,22 @@ const express= require('express')
 const app=express()
 
 app.set('view engine','ejs')
-
+app.use(express.urlencoded({extended:false}))
+app.use(express.static('public'))
 app.get('/',(req,res)=>{
+    res.render('Login')
+})
+app.get('/Reg',(req,res)=>{
+    res.render('Reg')
+})
+app.get('/index',(req,res)=>{
     res.render('index')
 })
 app.get('/index2',(req,res)=>{
     res.render('index2')
+})
+app.get('/Reg',(req,res)=>{
+    res.render('Reg')
 })
 app.get('/user/:username',(req,res)=>{
     res.render(`user`,{
