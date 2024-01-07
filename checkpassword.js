1c7049913c9f0eef48f6afbdb249351e85aeda5c
L.A <lazarenkov01@gmail.com>
2024-01-08 02:39:35 +0300

    dev 0.0.2

---
+++ checkpassword.js
@@ -0,0 +1,13 @@
+
+export class UserData {
+    constructor(username, password) {
+        this.username = username;
+        this.password = password
+        ;
+    }
+}
+
+
+
+
+console.log(UserData)
--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const express= require('express')
+/*import {UserData} from './checkpassword'*/
 
+const express= require('express')
 const app=express()
+
+class UserData {
+    constructor(name, passw) {
+        this.name = name;
+        this.passw = passw
+        ;
+    }
+}
 
 app.set('view engine','ejs')
 app.use(express.urlencoded({extended:false}))
@@ -17,14 +26,26 @@ app.get('/index',(req,res)=>{
 app.get('/index2',(req,res)=>{
     res.render('index2')
 })
-app.get('/Reg',(req,res)=>{
-    res.render('Reg')
-})
-app.get('/user/:username',(req,res)=>{
+
+/*app.get('/user/:username',(req,res)=>{
     res.render(`user`,{
         username:req.params.username,
         hobbies:[]
     })
+})*/
+app.post('/check-userdata',(req,res)=>
+{
+    let username=req.body.username
+    let password=req.body.password
+    let Test = new UserData("0","11")
+
+    if((Test.name==this.username)&&(Test.passw==this.password)) {
+            return res.redirect('/index')
+    }
+    else
+    {
+            return res.redirect('/')
+    }
 })
 const PORT=3001
 app.listen(PORT,()=>{
